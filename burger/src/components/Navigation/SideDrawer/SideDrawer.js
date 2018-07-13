d66b336c43cef57977a018a1d3bf720806a9876e
shiraz <mirzasbaig@nyu.edu>
2018-07-12 21:52:04 -0400

    Section 8

--- burger/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,17 +6,29 @@ import Logo from '../../Logo/Logo';
 
 import NavigationItems from '../NavigationItems/NavigationItems';
 
+import Backdrop from '../../UI/Backdrop/Backdrop';
+
+import Aux from '../../../hoc/Aux/Aux';
+
 const sideDrawer = (props) => {
-    // ..
+
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     );
 };
 
--- burger/src/components/Navigation/Toolbar/Toolbar.js
+++ burger/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 import classes from './Toolbar.css';
 
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+
 import Logo from '../../Logo/Logo';
 
 import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <DrawerToggle clicked={props.drawToggleClicked} />
         <div className={classes.Logo}>
             <Logo />
         </div>
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
