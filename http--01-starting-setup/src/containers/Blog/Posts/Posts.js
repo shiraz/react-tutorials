46033865dc6c6c9e6619f1b4ba466adc99c384a8
shiraz <mirzasbaig@nyu.edu>
2018-07-21 18:23:20 -0400

    Section 11 until Tutorial #179

---
+++ http--01-starting-setup/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+import axios from "../../../axios";
+
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+
+class Posts extends Component {
+
+    state = {
+        posts: [],
+        selectedPostId: null,
+        error: false
+    };
+
+    componentDidMount () {
+        console.log(this.props);
+        axios.get('/posts').then(response => {
+            const posts = response.data.slice(0, 4);
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Max'
+                }
+            });
+            this.setState({posts: updatedPosts});
+        }).catch(error => {
+            console.log(error);
+            //this.setState({error: true});
+        });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
+    render() {
+
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return (
+                    <Link to={`/${post.id}`} key={post.id}>
+                        <Post title={post.title}
+                              author={post.author}
+                              clicked={() => this.postSelectedHandler(post.id)} />
+                    </Link>);
+            });
+        }
+
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        );
+    }
+}
+
+export default Posts;
