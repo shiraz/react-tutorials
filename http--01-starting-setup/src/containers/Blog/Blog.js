d81b38dbfd0b3834706ad9512a9dc096bdad1b27
shiraz <mirzasbaig@nyu.edu>
2018-07-25 22:55:35 -0400

    Everything until tutorial 203

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import {Route, NavLink} from 'react-router-dom';
-
-import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
+// import axios from 'axios';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
+import Posts from './Posts/Posts';
+//import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
+
+    state = {
+      auth: false
+    };
 
     render () {
         return (
@@ -15,28 +23,31 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li>
-                                <NavLink
-                                    to="/"
-                                    exact
-                                    activeClassName="my-active"
-                                    activeStyle={{
-                                        color: '#fa923f',
-                                        textDecoration: 'underline'
-                                    }}>Home</NavLink></li>
+                            <li><NavLink
+                                to="/posts/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
-                                search: '?quick-submit: true'
+                                search: '?quick-submit=true'
                             }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
-                {/*<Route path="/" exact render={() => <h1>Home</h1>} />*/}
-                {/*<Route path="/" render={() => <h1>Home II</h1>} />*/}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+                <Switch>
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
+                    <Route path="/posts" component={Posts} />
+                    <Route render={() => <h1>Not found</h1>} />
+                    {/*<Redirect render="/" to="/posts" />*/}
+                    {/*<Redirect from="/" to="/posts" />*/}
+                </Switch>
             </div>
         );
     }
--- http--01-starting-setup/src/containers/Blog/NewPost/NewPost.js
+++ http--01-starting-setup/src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Redirect} from 'react-router-dom';
 
 import './NewPost.css';
 
@@ -7,10 +8,12 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
+        submitted: false
     };
 
     componentDidMount () {
+        // If unauth => this.props.history.replace('/posts');
         console.log(this.props);
     }
 
@@ -24,12 +27,20 @@ class NewPost extends Component {
 
         axios.post('/posts', data).then(response => {
             console.log(response);
+            //this.setState({submitted: true});
+            this.props.history.replace('/posts');
+            //this.props.history.push('/posts');
         });
     };
 
     render () {
+        let redirect = null;
+        if (this.state.submitted) {
+            redirect = <Redirect to="/posts" />
+        }
         return (
             <div className="NewPost">
+                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
