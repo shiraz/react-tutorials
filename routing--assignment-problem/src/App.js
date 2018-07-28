4d67f2aa483f0607ed6cb46757bf662c5d546dc2
shiraz <mirzasbaig@nyu.edu>
2018-07-27 20:02:27 -0400

    Challenge from section 11

---
+++ routing--assignment-problem/src/App.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, NavLink, Switch} from "react-router-dom";
+
+import Courses from './containers/Courses/Courses';
+import Users from './containers/Users/Users';
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <ol style={{textAlign: 'left'}}>
+                        <li>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without
+                            Links)
+                        </li>
+                        <li>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"
+                        </li>
+                        <li>Make the courses in "Courses" clickable by adding a link and load the "Course" component in
+                            the
+                            place of "Courses" (without passing any data for now)
+                        </li>
+                        <li>Pass the course ID to the "Course" page and output it there</li>
+                        <li>Pass the course title to the "Course" page - pass it as a param or score bonus points by
+                            passing
+                            it as query params (you need to manually parse them though!)
+                        </li>
+                        <li>Load the "Course" component as a nested component of "Courses"</li>
+                        <li>Add a 404 error page and render it for any unknown routes</li>
+                        <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
+                    </ol>
+
+                    <header>
+                        <nav>
+                            <ul className="mainLinks">
+                                <li>
+                                    <NavLink
+                                        to="/courses/"
+                                        exact
+                                        activeClassName="my-active"
+                                        activeStyle={{
+                                            color: '#fa923f',
+                                            textDecoration: 'underline'
+                                        }}>Courses</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/users/">Users</NavLink>
+                                </li>
+                            </ul>
+                        </nav>
+                    </header>
+
+                    <Switch>
+                        <Route path="/courses" component={Courses}/>
+                        <Route path="/users" component={Users}/>
+                    </Switch>
+
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default App;
