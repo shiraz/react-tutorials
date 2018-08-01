eb9dd5929482c9cd4ad2e4ba6104af32c10281ca
shiraz <mirzasbaig@nyu.edu>
2018-07-31 23:57:18 -0400

    Backup

--- routing--assignment-problem/src/containers/Course/Course.js
+++ routing--assignment-problem/src/containers/Course/Course.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
 class Course extends Component {
+
+    componentDidMount() {
+        console.log('test2');
+        console.log(this.state);
+    }
+
     render () {
         return (
             <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+                <h1>{this.props.title}</h1>
+                <p>You selected the Course with ID: {this.props.match.params.id}</p>
             </div>
         );
     }
--- routing--assignment-problem/src/containers/Courses/Courses.js
+++ routing--assignment-problem/src/containers/Courses/Courses.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import {Link, Route} from 'react-router-dom';
 
 import './Courses.css';
+
+import Course from '../Course/Course';
 
 class Courses extends Component {
     state = {
@@ -11,6 +14,15 @@ class Courses extends Component {
         ]
     }
 
+    componentDidMount() {
+        this.setState(this.state.courses);
+    }
+
+    courseSelectHandler = (id) => {
+        this.props.history.push(`/courses/${id}`);
+    }
+
+
     render () {
         return (
             <div>
@@ -18,10 +30,15 @@ class Courses extends Component {
                 <section className="Courses">
                     {
                         this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
+                            return <Link to={`/courses/${course.id}`} key={course.id}>
+                                    <article className="Course" key={course.id} title={course.title}>{course.title}</article>
+                                </Link>;
                         } )
                     }
                 </section>
+                {/*<Route path={this.props.match.url +  '/:id'} exact component={Course} />*/}
+                <Route exact path={this.props.match.url +  '/:id'} exact render={(props) => <Course {...props} /> } />
+
             </div>
         );
     }
