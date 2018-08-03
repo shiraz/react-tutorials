cae857eab8d42d5e2799469e15453ed5219fda67
shiraz <mirzasbaig@nyu.edu>
2018-08-03 01:30:17 -0400

    Completed question 4 and 5 from assignment 3

--- routing--assignment-problem/src/containers/Course/Course.js
+++ routing--assignment-problem/src/containers/Course/Course.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
+import queryString from 'query-string';
 
 class Course extends Component {
-
-    componentDidMount() {
-        console.log('test2');
-        console.log(this.state);
-    }
 
     render () {
         return (
             <div>
-                <h1>{this.props.title}</h1>
+                <h1>{queryString.parse(this.props.location.search).title}</h1>
                 <p>You selected the Course with ID: {this.props.match.params.id}</p>
             </div>
         );
--- routing--assignment-problem/src/containers/Courses/Courses.js
+++ routing--assignment-problem/src/containers/Courses/Courses.js
@@ -6,22 +6,14 @@ import './Courses.css';
 import Course from '../Course/Course';
 
 class Courses extends Component {
+
     state = {
         courses: [
             { id: 1, title: 'Angular - The Complete Guide' },
             { id: 2, title: 'Vue - The Complete Guide' },
             { id: 3, title: 'PWA - The Complete Guide' }
         ]
-    }
-
-    componentDidMount() {
-        this.setState(this.state.courses);
-    }
-
-    courseSelectHandler = (id) => {
-        this.props.history.push(`/courses/${id}`);
-    }
-
+    };
 
     render () {
         return (
@@ -30,8 +22,8 @@ class Courses extends Component {
                 <section className="Courses">
                     {
                         this.state.courses.map( course => {
-                            return <Link to={`/courses/${course.id}`} key={course.id}>
-                                    <article className="Course" key={course.id} title={course.title}>{course.title}</article>
+                            return <Link to={`/courses/${course.id}?title=${encodeURIComponent(course.title)}`} key={course.id}>
+                                    <article className="Course" key={course.id}>{course.title}</article>
                                 </Link>;
                         } )
                     }
