3506c379c1c82f66ad73ea1fdc3ab016809bb5ab
mirzasb <mirzasbaig@nyu.edu>
2018-09-27 22:47:39 -0400

    Section 18 start

--- burger/src/components/Navigation/NavigationItems/NavigationItems.js
+++ burger/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,6 +8,7 @@ const NavigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" exact>Burger Builder</NavigationItem>
         <NavigationItem link="/orders">Orders</NavigationItem>
+        <NavigationItem link="/auth">Authenticate</NavigationItem>
     </ul>
 );
 
